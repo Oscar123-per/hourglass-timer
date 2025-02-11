93742c6e9f47d9ea372ed6209e74ba3ef7193835
Oscar123-per <scunny123@icloud.com>
2025-02-12 08:34:02 +0900

    Added changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "48d1c713cb0a57b785b6f36e6680c47b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "6f807282d326a3fe847e42de21cdf718",
+"flutter_bootstrap.js": "a10744445200b9620d9b8595918db75c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
